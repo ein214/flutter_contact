71a1c10404a7def831dc5a0f94a8d72da7ff82bf
고은지 <ein214@wizzpay.co.kr>
2022-02-09 14:12:16 +0900

    6. 커스텀위젯, ListView

--- lib/main.dart
+++ lib/main.dart
@@ -15,90 +15,35 @@ class MyApp extends StatelessWidget {
     //코드는 이 아래서부터 작업
     //Flutter 앱 디자인은 위젯 짜집기
     return MaterialApp(
-      //home : Text('안녕')
-      //home : Icon(Icons.shop)
-      //home : Image.asset('thumb6563e826765f1f44e72a16fdb91e7b02.png')
-      //home : Container( width: 50, height: 50, color: Colors.amber )
-      /*
-      home : Center(
-        child: Container( width: 100, height: 100, color: Colors.amber ),
-      )
-       */
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height:150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('test2.PNG',  width: 150,),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라팝니다'),
-                    Text('금호동 3가'),
-                    Text('7000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-        )
-        /*
-        body: Row(
+        bottomNavigationBar: BottomAppBar(),
+        body: ListView(
           children: [
-            Expanded(child: Container(color: Colors.blue)),
-            Container(width:100, child: Container(color: Colors.red)),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),Text('안녕'),Text('안녕'),
+            Text('안녕'),
+            Text('안녕'),Text('안녕'),Text('안녕'),Text('안녕'),
+            Text('안녕'),Text('안녕'),Text('안녕'),Text('안녕'),Text('안녕'),Text('안녕'),
           ],
         ),
+      ),
+    );
+  }
+}
 
-         */
-        /*
-          body: Container(
-            width: double.infinity,
-            height: 120,
-            margin: EdgeInsets.all(5),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Image.asset('test2.PNG'),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)'),
-                        Text('성동구 행당동·끌올 10분 전', style: TextStyle(color : Colors.grey, fontSize: 11),),
-                        Text('210,000원', style: TextStyle( fontWeight: FontWeight.bold, fontSize: 13)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Icon(Icons.favorite_border),
-                            Text('4')
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            )
-          )
+/* stless 누르고 Tab키 누르고 사용 할 이름 추가 */
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
 
-         */
-        )
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+
+    // 이부분에 함수 작업
+    return SizedBox(
+        child: Text('안녕')
+    );
+  }
 }
