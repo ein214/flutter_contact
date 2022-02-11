c46f3f2691e0ba46550ef339426461f79817a5e5
고은지 <ein214@wizzpay.co.kr>
2022-02-11 13:48:47 +0900

    11장 응용해보기 - 빈칸으로 완료버튼 누르면 추가안되게?
    - 이름옆에 삭제버튼과 기능?
    
    - 이름들 가나다순 정렬버튼? (sort함수 사용법을 찾아봅시다)
    
    - 전화번호 데이터도 3개 마련해놓고 전화번호도 보여주고 싶으면?

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+          home: MyApp()
+      )
+  );
 }
 
 
@@ -14,9 +19,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
-  var name = ['고은지', '정수연', '곽청'];
-  var like = [0, 0, 0];
+  var name = [['고은지', '01022223333'], ['정수연', '01022223333'], ['곽청','01022223333']];
+  var total = 3;
+
+  addOne() {
+    setState(() {
+      total++;
+    });
+  }
+
+  //과제 문제점 자식쪽에서 바꿀게 아니라 부모쪽에서 바꿀함수를 만들어서 보내는게 맞음
+  addName(inputName) {
+    setState(() {
+      name.add(inputName);
+    });
+  }
+
+  removeOne(index) {
+    setState(() {
+      name.removeAt(index);
+    });
+  }
+
+  sortName() {
+    setState(() {
+      name.sort();
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,27 +54,97 @@ class _MyAppState extends State<MyApp> {
     //위쪽은 기본세팅 같은것
     //코드는 이 아래서부터 작업
     //Flutter 앱 디자인은 위젯 짜집기
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar( title: Text('앱')),
-        body: ListView.builder(
-            itemCount: 3,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: Text(like[index].toString()),
-                title: Text(name[index]),
-                trailing: TextButton(
-                  child: Text('좋아요'),
-                  onPressed: (){
-                    setState(() {
-                      like[index]++;
-                    });
-                  },
-                ),
-              );
-            }
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            //print(context.findAncestorWidgetOfExactType<MaterialApp>());
+            showDialog(context: context, builder: (context) {
+                return DialogUI( addOne : addOne, addName : addName );
+            });
+          },
         ),
-      ),
+        appBar: AppBar( title: Text(total.toString())),
+        body: SizedBox(
+          height:500,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(onPressed: (){
+                sortName();
+              }, icon: Icon(Icons.sort)),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: name.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Icon(Icons.account_circle, size: 36,),
+                        title: Text(name[index][0]),
+                        subtitle: Text(name[index][1]),
+                        trailing: IconButton(icon: Icon(Icons.close), onPressed: () {
+                          removeOne(index);
+                        },),
+                      );
+                    },
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+
+      );
+  }
+}
+
+class DialogUI extends StatelessWidget {
+   DialogUI({Key? key, this.addOne, this.addName}) : super(key: key);
+   final addOne;
+   final addName;
+   var inputData = TextEditingController();
+   var inputPhone = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+        child: Container(
+            width: double.infinity,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Contact', style: TextStyle(fontWeight: FontWeight.bold),),
+                  TextField( controller: inputData, decoration: InputDecoration( labelText: '이름'),),
+                  TextField( controller: inputPhone, decoration: InputDecoration( labelText: '연락처'), maxLength: 11,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(onPressed: () {
+                        Navigator.pop(context, true);
+                      }, child: Text('Cancel')),
+                      TextButton(onPressed: (){
+                        if (inputData.text.isEmpty == true) {
+                          showDialog(barrierDismissible: false, context: context, builder: (context) {
+                              return AlertDialog(content: Text("이름을 입력하세요"), actions: [
+                                TextButton(child: Text("확인"), onPressed: (){
+                                  Navigator.pop(context);
+                                },)
+                              ],);
+                          });
+                        } else {
+                          addOne();
+                          addName([inputData.text, inputPhone.text]);
+                          Navigator.pop(context, true);
+                        }
+                      }, child: Text('완료'))
+                    ],
+                  ),
+                ]
+            )
+        )
     );
   }
 }
